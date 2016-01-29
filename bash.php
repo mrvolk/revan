4fe0e8ee799cce6f040c844d113c27c89548d671
Revan <revandy.satria45@gmail.com>
2016-01-29 18:07:39 +0800

    test

---
+++ bash.php
@@ -0,0 +1,20 @@
+<!DOCTYPE html>
+<html>
+    <head>
+		<title>A loop of your own</title>
+        <link type='text/css' rel='stylesheet' href='style.css'/>
+	</head>
+	<body>
+    <?php
+	//Add while loop below
+    $i = true;
+    while($i == true){
+        echo "<p>LOOP JALAN</p>";
+        
+        $i = false;
+        echo "<p>LOOP 2</p>";
+        }
+        
+    ?>
+    </body>
+</html>
